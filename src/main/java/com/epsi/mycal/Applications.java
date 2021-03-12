cde692b10f95d284eec321dc7579b1452906e14c
DESKTOP-GO1SN2Q\shrik <jeanmichelsoler44@gmail.com>
2021-03-12 10:34:02 +0100

    upgrad quality of code

--- src/main/java/com/epsi/mycal/Applications.java
+++ src/main/java/com/epsi/mycal/Applications.java
@@ -1,7 +1,7 @@
 package com.epsi.mycal;
 
 public class Applications {
-    static void main(String[] args) {
-        System.out.println("Hello World !");
-    }
+	static void main(String[] args) {
+		System.out.println("Hello World !");
+	}
 }
--- src/main/java/com/epsi/mycal/Calculator.java
+++ src/main/java/com/epsi/mycal/Calculator.java
@@ -1,10 +1,11 @@
 package com.epsi.mycal;
 
 public class Calculator {
-    public int add(int a, int b){
-        return a + b;
-    }
-    public int sub(int a, int b){
-        return a - b;
-    }
+	public int add(int valueA, int valueB) {
+		return valueA + valueB;
+	}
+
+	public int sub(int valueA, int valueB) {
+		return valueA - valueB;
+	}
 }
